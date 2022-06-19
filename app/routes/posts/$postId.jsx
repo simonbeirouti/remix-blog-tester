a32a842cb0f704e3a935da42b25089c31b2a4a5b
simonbeirouti <hello@simonbeirouti.com>
2022-06-20 00:29:10 +1000

    add check for userid and hide button

--- app/routes/posts/$postId.jsx
+++ app/routes/posts/$postId.jsx
@@ -1,15 +1,18 @@
 import { redirect } from "@remix-run/node"
 import { useLoaderData, Link } from "@remix-run/react"
 import { db } from '~/utils/db.server'
+import { getUser } from '~/utils/session.server'
 
-export const loader = async ({ params }) => {
+export const loader = async ({ request, params }) => {
+    const user = await getUser(request)
+
     const post = await db.post.findUnique({
         where: { id: params.postId }
     })
 
     if(!post) throw new Error('Post not found')
 
-    const data = { post }
+    const data = { post, user }
 
     return data
 }
@@ -18,13 +21,17 @@ export const action = async ({ request, params }) => {
     const form = await request.formData()
 
     if(form.get('_method') === 'delete') {
+        const user = await getUser(request)
+
         const post = await db.post.findUnique({
             where: { id: params.postId }
         })
 
         if(!post) throw new Error('Post not found')
 
-        await db.post.delete({ where:  { id: params.postId } })
+        if(user && post.userId === user.id) {
+            await db.post.delete({ where:  { id: params.postId } })
+        }
 
         return redirect('/posts')
     }
@@ -32,7 +39,7 @@ export const action = async ({ request, params }) => {
 
 
 export default function Post() {
-    const { post } = useLoaderData()
+    const { post, user } = useLoaderData()
   
     return (
         <>
@@ -46,12 +53,14 @@ export default function Post() {
             </div>
 
             <div className="page-footer">
-                <form method="POST">
-                    <input type="hidden" name="_method" value="delete" />
-                    <button className="btn btn-delete">
-                        Delete
-                    </button>
-                </form>
+                {user.id === post.userId && (
+                    <form method="POST">
+                        <input type="hidden" name="_method" value="delete" />
+                        <button className="btn btn-delete">
+                            Delete
+                        </button>
+                    </form>
+                )}
             </div>
         </>
     )
