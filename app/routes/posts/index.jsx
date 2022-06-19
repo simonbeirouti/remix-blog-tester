a40cca6e8e72f828049406c8e1e74ebc2412dcd4
simonbeirouti <hello@simonbeirouti.com>
2022-06-19 13:24:53 +1000

    create posts page with dummy content

---
+++ app/routes/posts/index.jsx
@@ -0,0 +1,34 @@
+import { useLoaderData, Link } from "@remix-run/react"
+
+export const loader = () => {
+    const data = {
+        posts: [
+            {id: 1, title: 'Post 1', body: 'This is post 1'},
+            {id: 2, title: 'Post 2', body: 'This is post 2'},
+            {id: 3, title: 'Post 3', body: 'This is post 3'},
+        ]
+    }
+
+    return data
+}
+
+export default function PostItems() {
+    const { posts } = useLoaderData()
+  return (
+    <>
+        <div className="page-header">
+            <h1>Posts</h1>
+            <Link to="/posts/new" className="btn">New Post</Link>
+        </div>
+        <ul className="posts-list">
+            {posts.map((post) => (
+                <li key={post.id}>
+                    <Link to={post.id}>
+                        <h3>{post.title}</h3>
+                    </Link>
+                </li>
+            ))}
+        </ul>
+    </>
+  )
+}
