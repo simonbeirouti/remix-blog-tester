cfc45a93e668be615834ae6a10af81d09420ebc2
simonbeirouti <hello@simonbeirouti.com>
2022-06-19 14:06:18 +1000

    pull post data from db and display the date

--- app/routes/posts/index.jsx
+++ app/routes/posts/index.jsx
@@ -1,12 +1,13 @@
 import { useLoaderData, Link } from "@remix-run/react"
+import { db } from '~/utils/db.server'
 
-export const loader = () => {
+export const loader = async () => {
     const data = {
-        posts: [
-            {id: 1, title: 'Post 1', body: 'This is post 1'},
-            {id: 2, title: 'Post 2', body: 'This is post 2'},
-            {id: 3, title: 'Post 3', body: 'This is post 3'},
-        ]
+        posts: await db.post.findMany({
+            take: 20,
+            select: {id: true, title: true, createdAt: true},
+            orderBy: {createdAt: 'desc'}
+        }),
     }
 
     return data
@@ -25,6 +26,7 @@ export default function PostItems() {
                 <li key={post.id}>
                     <Link to={post.id}>
                         <h3>{post.title}</h3>
+                        {new Date(post.createdAt).toLocaleString()}
                     </Link>
                 </li>
             ))}
